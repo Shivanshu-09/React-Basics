c0e49d5c2e20c2ee8f54e4b7773a5994f9410782
Shivanshu <shivanshu.sarin@connectwise.com>
2023-04-27 20:57:01 +0530

    Spread Operator

---
+++ Spread/arrays.js
@@ -0,0 +1,21 @@
+// let arr = [1,2,3];
+
+// arr2 = arr;
+
+// arr2.push(4);
+
+// console.log(arr); //  [1,2,3,4]
+// console.log(arr2); //  [1,2,3,4]
+
+// Spread Operator
+
+let arr = [1,2,3];
+let arr2 = [...arr];
+arr2.push(4);
+console.log(arr); // [1,2,3]
+console.log(arr2); // [1,2,3,4]
+
+
+
+
+
